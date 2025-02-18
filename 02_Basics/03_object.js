f3e61da8b016a0206461fbdbf5f8d99dbdfd8e2e
VN45 <vishalnagashankar4@gmail.com>
2025-02-18 08:03:02 +0000

    till arrays completed objects started

---
+++ 02_Basics/03_object.js
@@ -0,0 +1,22 @@
+// singleton
+// Object.create
+
+//object literals
+const JsUser = {
+    name: "Hitesh", //key and value and be defined and accesed 
+    age: 18,
+    location: "Jaipur",
+    email: "vishal@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+//object-access
+
+console.log(JsUser.email);
+console.log(JsUser[email])
+
+
+
+
+
